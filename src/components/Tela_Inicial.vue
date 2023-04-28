14d6d56766e0f366c70165c5c6b1fe133c7cdaa6
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-04-27 23:33:08 -0300

    alterando css[

--- src/components/Tela_Inicial.vue
+++ src/components/Tela_Inicial.vue
@@ -10,8 +10,10 @@
     <div id="tamplate">
       
     </div>
+    <!-- Div que eu quero mudar -->
+    <div :id="Info_Ocu">
+        <h1 class="h1_ocupacao">Ocupação atual</h1>
 
-    <div id="Info_Ocu">
         <div class="Info_Ocu_di">
 
         </div>
@@ -23,6 +25,11 @@
 
     <div id="Skills">
 
+      <h1 class="h1_skills">Minhas Skills e Power up's</h1>
+
+      <div class="Info_Ocu_Skills">
+
+      </div>
     </div>
 
     <div id="Contato">
@@ -32,17 +39,41 @@
 </template>
 
 <script>
-import rodape from './rodape.vue'
+  import rodape from './rodape.vue'
 
-export default {
-  name: 'Tela_Inicial',
-  components:{
-    rodape
+  export default {
+    name: 'Tela_Inicial',
+    components:{
+      rodape
+    },
+
+    data(){
+      return{
+        scrollPosition: null 
+      }
+    },
+
+    methods:{
+
+        Alterar_Background: function(){
+          
+          var div_alterada = document.getElementById("#Info_Ocu");
+          var ponto_maximo = '50';
+
+          window.addEventListener('scroll', function(){
+
+            if(window.scrollY > ponto_maximo){
+
+              div_alterada.classList.add('mudar_estilo');
+            }else{
+              div_alterada.classList.remove('mudar_estilo');
+            }
+          })
+        }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style src="./style.css">
-
 </style>
