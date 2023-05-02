bc422d99873fdde164fc427b9d2db5b13c1497a7
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-05-02 16:07:44 -0300

    Criando sistema de envio de email

--- src/backend/EmailSettings.php
+++ src/backend/EmailSettings.php
@@ -1,30 +1,29 @@
 <?php
 header('Content-Type: application/json');
 
-// Obter os dados do formulário
+
 $nome = $_POST['nome'];
 $sobrenome = $_POST['sobrenome'];
 $email = $_POST['email'];
+$telefone = $_POST['telefone'];
 $message = $_POST['mensagem'];
 
-// Validar os dados do formulário
-if (empty($nome) || empty($sobrenome) || empty($email) || empty($mensagem)) {
-  // Se algum dos campos estiver vazio, retorne uma mensagem de erro
+if (empty($nome) || empty($sobrenome) || empty($email) || empty($telefone) || empty($mensagem)) {
+
   echo json_encode(array('success' => false, 'message' => 'Por favor, preencha todos os campos.'));
   exit;
 }
 
-// Configurar as informações do e-mail
 $to = 'caldeiran391@gmail.com';
 $subject = 'Nova mensagem do formulário de contato';
-$body = "Nome: $nome\n   Sobrenome: $sobrenome\n   E-mail: $email\n  Mensagem:\n$mensagem";
+$body = "Nome: $nome\n   Sobrenome: $sobrenome\n   E-mail: $email\n  Telefone: $telefone\n Mensagem:\n$mensagem";
 
-// Enviar o e-mail
+
 if (mail($to, $subject, $body)) {
-  // Se o e-mail for enviado com sucesso, retorne uma mensagem de sucesso
+
   echo json_encode(array('success' => true, 'message' => 'Mensagem enviada com sucesso!'));
 } else {
-  // Se ocorrer um erro ao enviar o e-mail, retorne uma mensagem de erro
+
   echo json_encode(array('success' => false, 'message' => 'Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.'));
 }
 ?>
--- src/components/Contato/Contato.vue
+++ src/components/Contato/Contato.vue
@@ -1,16 +1,30 @@
 <template>
-    <input type="text" id="input" class="input_nome"  placeholder="Seu nome">
-    <input type="text" id="input" class="input_sobrenome" placeholder="Sobrenome">
-    <br><input type="text" id="input" class="input_telefone" placeholder="telefone">
-    <input type="text" id="input" class="input_email" placeholder="E-mail">
-    <textarea id="text_area" placeholder="Assunto">
+    <input type="text" id="input" class="input_nome"   v-model="form_Data.nome"  placeholder="Seu nome">
+    <input type="text" id="input" class="input_sobrenome" v-model="form_Data.sobrenome" placeholder="Sobrenome">
+    <br><input type="text" id="input" class="input_telefone" v-model="form_Data.telefone"  placeholder="telefone">
+    <input type="text" id="input" class="input_email"  v-model="form_Data.assunto" placeholder="E-mail">
+    <textarea id="text_area" v-model="form_Data.assunto" placeholder="Assunto">
 
     </textarea>
 </template>
 
 <script>
 export default{
-    name: 'Contato'
+    name: 'Contato',
+    data(){
+        return{
+            form_Data:{
+                nome: "",
+                sobrenome: "",
+                email: "",
+                telefone: null,
+                assunto: ""
+            }
+        }
+    },
+    methods:{
+            
+    }
 }
 
 
